f68bf07648a54ebf3a00e0653f698b98f986446c
AlessandroSvr12 <alessandrosvr12@gmail.com>
2026-01-07 17:01:06 +0100

    seconda versione del codice

--- UI/controller.py
+++ UI/controller.py
@@ -9,33 +9,41 @@ class Controller:
 
     def handle_crea_grafo(self, e):
         """ Handler per gestire creazione del grafo """""
+        if self._view.dd_anno.value is None:
+            self._view.show_alert("anno invalido")
+        else:
+            anno=self._view.dd_anno.value
+            self._model.crea_grafo(anno)
 
-        anno=self._view.dd_anno.value
-        self._model.crea_grafo(anno)
-
-        for nodo in self._model.G.nodes():
-            #ricordati che python fa giochi strani mettendo nodo come value
-                self._view.dd_squadra.options.append( ft.dropdown.Option( key=nodo.team_code, text=f"{nodo.team_code} ({nodo.nome})" ) )
-        self._view.page.update()
+            for nodo in self._model.G.nodes():
+                #ricordati che python fa giochi strani mettendo nodo come value
+                    self._view.dd_squadra.options.append( ft.dropdown.Option( key=nodo.team_code, text=f"{nodo.team_code} ({nodo.nome})" ) )
+            self._view.page.update()
 
     def handle_dettagli(self, e):
         """ Handler per gestire i dettagli """""
-
-        risultati=self._model.analisi_dettagli(self._view.dd_squadra.value)
-        self._view.txt_risultato.clean()
-        for i in risultati:
-            self._view.txt_risultato.controls.append(ft.Text(i))
-        self._view.page.update()
+        if self._view.dd_squadra.value is None:
+            self._view.show_alert("squadra non valida")
+        else:
+            risultati=self._model.analisi_dettagli(self._view.dd_squadra.value)
+            self._view.txt_risultato.clean()
+            for i in risultati:
+                self._view.txt_risultato.controls.append(ft.Text(f"{i[0].nome}  peso: {i[1]}"))
+            self._view.page.update()
 
 
     def handle_percorso(self, e):
         """ Handler per gestire il problema ricorsivo di ricerca del percorso """""
+        if self._view.dd_squadra.value is None:
+            self._view.show_alert("squadra non valida")
+        else:
+            percorso_best=self._model.crea_percorso(self._view.dd_squadra.value)
+            self._view.txt_risultato.clean()
 
+            for i in percorso_best:
+                self._view.txt_risultato.controls.append(ft.Text(f"{i[0].nome}->{i[1].nome}  peso:{i[2]}"))
+            self._view.page.update()
 
-        percorso_best=self._model.crea_percorso(self._view.dd_squadra.value)
-        print(len(percorso_best))
-        for i in percorso_best:
-            print(i)
 
     """ Altri possibili metodi per gestire di dd_anno """""
     def handle_anno(self):
